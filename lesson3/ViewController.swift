63b388fdfc54df05028b33fcafe5773ae51055a7
voroshyn <voroshyn.it@gmail.com>
2024-04-22 02:07:14 +0300

    a new project has been created

---
+++ lesson3/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("стирай этот метод и пиши код сюда")
+    }
+}
